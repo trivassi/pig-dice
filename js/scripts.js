14849f38298693d22efa98bb5365a31e702bae57
Andrew Malkin and Thomas Rivas <thomas@>
2016-08-24 13:08:35 -0700

    create files and directories with start to backend logic and basic layout

---
+++ js/scripts.js
@@ -0,0 +1,56 @@
+//Back End
+function Player(username, playerNumber, turnScore, totalScore) {
+  this.username = username;
+  this.playerNumber = playerNumber;
+  this.turnScore = turnScore;
+  this.totalScore = totalScore;
+}
+
+function Dice(dice1, dice2) {
+  this.dice1 = dice1;
+  this.dice2 = dice2;
+}
+
+Dice.prototype.diceTotal = function() {
+  return this.dice1 + this.dice2;
+}
+
+var dice = new Dice(0, 0);
+var p1 = new Player("t1k1", 1, 0, 0);
+var p2 = new Player("trivassi", 2, 0, 0);
+var count = 0;
+
+
+
+function diceRoll() {
+  dice.dice1 = 1 + Math.floor(Math.random() * 6);
+  dice.dice2 = 1 + Math.floor(Math.random() * 6);
+
+  if ((count%2) === 0) {
+    if (dice.dice1 != 0 && dice.dice2 != 0) {
+      p1.turnScore += dice.diceTotal;
+      return p1.turnScore;
+    } else if (dice.dice1 === 1 || dice.dice2 === 1){
+      p1.turnScore = 0;
+      count += 1;
+      return p1.turnScore;
+    }
+  } else if ((count%2) === 1){
+    if (dice.dice1 != 0 && dice.dice2 != 0) {
+      p2.turnScore += dice.diceTotal;
+      return p2.turnScore;
+    } else if (dice.dice1 === 1 || dice.dice2 === 1){
+      p2.turnScore = 0;
+      count += 1;
+      return p2.turnScore;
+    }
+  }
+}
+
+
+
+
+$(document).ready(function() {
+  $("")
+
+});
