f47c33a7753800b138d4ad0fea73a131de62dd76
Andrew Malkin and Thomas Rivas <andrew@>
2016-08-24 14:46:50 -0700

    complete pig dice logic with two dice and two players being able to determine winner

--- js/scripts.js
+++ js/scripts.js
@@ -27,8 +27,8 @@ function diceRoll() {
   dice.dice2 = 1 + Math.floor(Math.random() * 6);
 
   if ((count%2) === 0) {
-    if (dice.dice1 != 0 && dice.dice2 != 0) {
-      p1.turnScore += dice.diceTotal;
+    if (dice.dice1 != 1 && dice.dice2 != 1) {
+      p1.turnScore += dice.diceTotal();
       return p1.turnScore;
     } else if (dice.dice1 === 1 || dice.dice2 === 1){
       p1.turnScore = 0;
@@ -36,8 +36,8 @@ function diceRoll() {
       return p1.turnScore;
     }
   } else if ((count%2) === 1){
-    if (dice.dice1 != 0 && dice.dice2 != 0) {
-      p2.turnScore += dice.diceTotal;
+    if (dice.dice1 != 1 && dice.dice2 != 1) {
+      p2.turnScore += dice.diceTotal();
       return p2.turnScore;
     } else if (dice.dice1 === 1 || dice.dice2 === 1){
       p2.turnScore = 0;
@@ -48,9 +48,42 @@ function diceRoll() {
 }
 
 
-
-
 $(document).ready(function() {
-  $("")
+  $("#roll").click(function(event) {
+    event.preventDefault();
+
+    $("#turn-score").text(diceRoll());
+
+  });
+
+  $("#end").click(function() {
+
+    if ((count%2) === 0) {
+      p1.totalScore += p1.turnScore;
+      $("#p1-total-score").text(p1.totalScore);
+    } else if ((count%2) === 1) {
+      p2.totalScore += p2.turnScore;
+      $("#p2-total-score").text(p2.totalScore);
+    }
+
+    count += 1;
+    p1.turnScore = 0;
+    p2.turnScore = 0;
+    $("#turn-score").text(0);
+
+    if (p1.totalScore >= 10) {
+      $(".playing").hide();
+      $(".won").show();
+      $("#username-won").text("Player 1 Wins");
+      $("#user-points-won").text(p1.totalScore);
+    } else if (p2.totalScore >= 10) {
+      $(".playing").hide();
+      $(".won").show();
+      $("#username-won").text("Player 2 Wins");
+      $("#user-points-won").text(p2.totalScore);
+    }
+  });
+
+
 
 });
